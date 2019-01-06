89d4fcb89f06a53e7ca8680c83b55e57f3b62e20
Angellica Araujo <angellica.c.a@gmail.com>
2019-01-06 11:09:07 -0200

    training changes

--- capstone/code/train2.py
+++ capstone/code/train2.py
@@ -40,30 +40,73 @@ orders_products = pandas.concat(frames)
 user_1_orders_products = orders_products[orders_products['user_id']==1]
 count_user_1_orders_products = user_1_orders_products.groupby(['order_id'])['order_id'].aggregate('count')
 
-# 3 : Features related with user interest
+# --------------------------------------------------------------------------------------------------------------------------------
 
-# 3.1 : Select reordered products
+# 2 : User Behavior :  Features related with user interest
+
+# 2.1 : Select reordered products
 products_reordered = orders_products[orders_products['reordered']==1]
 
-# 3.2 : Count reordered frequency for each user
+# 2.2 : Count reordered frequency for each user
 count_items_on_cart = products_reordered.groupby(['user_id'])['add_to_cart_order'].aggregate('count')
 count_items_on_cart = count_items_on_cart.reset_index(name='count_reorder_by_user')
 
 count_items_on_cart_user_1 = count_items_on_cart[count_items_on_cart['user_id']==1]
 
-# 3.3 : Count orders made for each user user
+# 2.3 : Count orders made for each user user
 count_orders_by_user = orders.groupby('user_id').size()
 count_orders_by_user = count_orders_by_user.reset_index(name='count_orders_by_user')
 
-# 3.4 : Count reordered frequency for each user by product
+# 2.4 : Count reordered frequency for each user by product
 count_user_reorder_by_product = products_reordered.groupby(by=['user_id','product_id'], as_index=False)['reordered'].aggregate('count')
-count_user_reorder_by_product = count_user_reorder_by_product.rename(columns={'reordered': 'count_reorder_product_by_user'})
+product_user_behavior = count_user_reorder_by_product.rename(columns={'reordered': 'count_reorder_product_by_user'})
 
-user_1_count_orders_by_user = count_user_reorder_by_product[count_user_reorder_by_product['user_id']==1]
+# 2.5 : Combine everything as user behavior
+general_user_behavior = pandas.merge(count_items_on_cart, count_orders_by_user, on='user_id', how='right')
 
-# 3.5 : Combine everything as user behavior
-user_behavior = pandas.merge(count_items_on_cart, count_orders_by_user, on='user_id', how='right')
-user_behavior = pandas.merge(user_behavior, count_user_reorder_by_product, on='user_id', how='right', validate='one_to_many')
+# 2.6 : Run the basic test again
+user_1_orders_products_on_user_behavior = product_user_behavior[product_user_behavior['user_id']==1]
 
-# 3.6 : Run the basic test again
-user_1_orders_products_on_user_behavior = user_behavior[user_behavior['user_id']==1]
+# 2.7 : Verify the existence for NaNs
+print("----------------------------- User Behavior : Verify the existence for NaNs ----------------------------------")
+print(product_user_behavior.isna().sum())
+
+# --------------------------------------------------------------------------------------------------------------------------------
+
+# 3 : Product Preferences : Preprocessing products set
+
+# 3.1 : Count general rerder interest by product
+count_general_reorder_by_product = orders_products.groupby(by=['product_id'], as_index=False)['reordered'].aggregate('count')
+count_general_reorder_by_product = count_general_reorder_by_product.rename(columns={'reordered':'count_general_item_reorder_freq'})
+
+# 3.2 : Combine as product preference
+products_preferences = pandas.merge(count_general_reorder_by_product, orders_products, on='product_id', how='right', validate='one_to_many')
+
+# 3.3 : Test total orders for user 1 
+user_1_products_preferences = products_preferences[products_preferences['user_id']==1]
+count_user_1_products_preferences = user_1_products_preferences.groupby(['order_id'])['order_id'].aggregate('count')
+
+# 3.4 : Verify the existence for NaNs
+print("----------------------------- Products Preferences : Verify the existence for NaNs ----------------------------------")
+print(products_preferences.isna().sum())
+
+# --------------------------------------------------------------------------------------------------------------------------------
+
+# 4 : Build Basket set
+
+baskets = pandas.merge(general_user_behavior, products_preferences, on=['user_id'], how='right', validate='one_to_many')
+baskets = pandas.merge(product_user_behavior, baskets, on=['user_id', 'product_id'], how='right')
+
+# 4.4 : Test total orders for user 1 
+user_1_baskets = baskets[baskets['user_id']==1]
+count_user_1_baskets = user_1_baskets.groupby(['order_id'])['order_id'].aggregate('count')
+
+# 4.2 : Verify the existence for NaNs
+print("----------------------------- Baskets : Verify the existence for NaNs ----------------------------------")
+print(baskets.isna().sum())
+
+# 4.3 : Update NaNs
+baskets['count_reorder_by_user'] = baskets['count_reorder_by_user'].fillna(0)
+
+
+# baskets = baskets.drop(['eval_set', 'add_to_cart_order', 'reordered', 'days_since_prior_order', 'order_dow', 'order_hour_of_day', 'order_id', 'order_number']) 
